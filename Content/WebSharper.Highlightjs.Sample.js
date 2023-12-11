92cba95474645937e3eada2c0a55c4f8b416b515
intellifactory-gh <intellifactory-gh@users.noreply.github.com>
2023-12-11 16:07:22 +0000

    deploy: 036332d37248d52c849ad9d59c8e816898c54d63

--- Content/WebSharper.Highlightjs.Sample.js
+++ Content/WebSharper.Highlightjs.Sample.js
@@ -1,6 +1,8 @@
+import { Start } from "./WebSharper.Core.JavaScript/Runtime.js"
 import { OnLoad } from "./WebSharper.Core.JavaScript/Runtime.js"
 function Main(){ }
 OnLoad(() => {
   Main();
 });
+Start();
 
