cb89cf8d968587244388483519dade31e0f308c9
Jand42 <Jand42@users.noreply.github.com>
2025-03-12 17:52:35 +0000

    deploy: 25cc621c04dd742ec2ce5a1057ddff121fee1bdd

--- Content/WebSharper.Highlightjs.Sample.js
+++
@@ -1,8 +0,0 @@
-import { Start } from "./WebSharper.Core.JavaScript/Runtime.js"
-import { OnLoad } from "./WebSharper.Core.JavaScript/Runtime.js"
-function Main(){ }
-OnLoad(() => {
-  Main();
-});
-Start();
-
