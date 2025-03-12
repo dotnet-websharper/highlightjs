25cc621c04dd742ec2ce5a1057ddff121fee1bdd
András Jankó <andras.janko@gmail.com>
2025-03-12 18:49:08 +0100

    updates for WS8

---
+++ WebSharper.Highlightjs.Sample/esbuild.config.mjs
@@ -0,0 +1,14 @@
+﻿import { build } from 'esbuild'
+
+var options =
+{
+  entryPoints: ['./build/WebSharper.Highlightjs.Sample.js'],
+  bundle: true,
+  minify: true,
+  format: 'iife',
+  outfile: '../dist/Scripts/WebSharper.Highlightjs.Sample.min.js',
+  globalName: 'wsbundle'
+};
+
+build(options);
+
